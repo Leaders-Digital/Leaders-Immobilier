6ae8b29bbcb71db21e2855091c446804e81e222a
punk010 <mohamed.amine.atouii@gmail.com>
2025-03-12 13:01:41 +0100

    removed console logs

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,10 @@ import Confidentialite from './components/confidentialité';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
+  console.log = () => {};
     return(
+    
+
         <Router>
         <Routes>
           <Route path="/login" element={<Login />} />
